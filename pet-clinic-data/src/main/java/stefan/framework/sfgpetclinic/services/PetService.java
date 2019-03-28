7f95bb04348bd11038cb94d23a5a62487a319149
witt3 <jans.stefan@gmail.com>
2019-03-28 18:51:05 +0100

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/stefan/framework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package stefan.framework.sfgpetclinic.services;
+
+import stefan.framework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
